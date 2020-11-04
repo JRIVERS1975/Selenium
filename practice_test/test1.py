deae871a3ba148cc275357c01d82d5b4b5393cfe
JRIVERS1975 <61665198+JRIVERS1975@users.noreply.github.com>
2020-11-04 00:12:48 -0700

    Initial commit

---
+++ practice_test/test1.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Chrome("../drivers/chromedriver")
+
+driver.set_page_load_timeout(10)
+
+driver.get("https://google.com")
+
+driver.find_element_by_name("q").send_keys("Automation step by step")
+driver.find_element_by_class_name("gNO89b").click()
+
+
+time.sleep(2)
+
+driver.close()
+driver.quit()
+
+print("Test Completed")
+
